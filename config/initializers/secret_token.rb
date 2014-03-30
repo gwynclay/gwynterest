faa353715823ef360f17688c38be14d0abb64b3b
gwynclay <gwynclay@gmail.com>
2014-03-30 09:53:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gwynterest::Application.config.secret_key_base = '2c78166791c31f40ca902326ad01c9328db041f57b4dd822b217d5d4e8bb858be7b44e761140a1a88689caeda56a71498e33924384e831ab6c5283435045ddfd'
